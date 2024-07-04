d2c615d42620549b8ae8e80b0e122a9f3ea30ac1
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-07-04 17:07:58 +0530

    adding admin

--- chaiourDjango/chai/views.py
+++ chaiourDjango/chai/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import ChaiVarity
+from .models import ChaiVariety
 from django.shortcuts import get_list_or_404
 
 # Create your views here.
 def all_chai(request):
-    chais = ChaiVarity.objects.all()
+    chais = ChaiVariety.objects.all()
     return render(request,"chai.html",{'chais':chais})
 
 def chai_detail(request,chai_id):
-    chai=get_list_or_404(ChaiVarity,pk=chai_id)
+    chai=get_list_or_404(ChaiVariety,pk=chai_id)
     return render (request,'chai_detail.html',{'chai':chai})
