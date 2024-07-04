31d215db7c962d7fcc85e5cbee65634ef7cab5b3
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-07-04 15:46:36 +0530

    models update

--- chaiourDjango/chai/models.py
+++ chaiourDjango/chai/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 from django.utils import timezone
 # Create your models here.
@@ -10,11 +11,14 @@ class ChaiVarity(models.Model):
         ('PL','PLAIN'),
         ('EL','ELACHI'),
     ]
-    name=models.CharField(max_length=100)
-    image=models.ImageField(upload_to='chars/')
-    date_added=models.DateTimeField(default=timezone)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chars/')
+    date_added = models.DateTimeField(default=timezone.now)
 
-    type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
+    type = models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
 
     description = models.TextField(default='')
+
+    def __str__(self,):
+        return self.name , self.date_added
 
